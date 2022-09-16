f8ab5d82e3b463917dad02aee2d6273fe8b78e02
Brandon Bruno <brab@sitecore.net>
2022-09-16 09:11:30 -0400

    Added Session News component.

---
+++ Website/src/rendering/src/components/Sessions/SessionNews.tsx
@@ -0,0 +1,25 @@
+import { RichText, Field } from '@sitecore-jss/sitecore-jss-nextjs';
+
+export type SessionNewsProps = {
+  News: Field<string>;
+};
+
+const SessionNews = (props: SessionNewsProps): JSX.Element => {
+  
+  return (
+    <>
+      <section className="section information-section">
+      
+        <div className="information-grid">
+          <div className="main-col">
+            <div className="column-title">News &amp; Updates for this session:</div>
+              <RichText field={props?.News} />
+          </div>
+        </div>
+        
+      </section>
+    </>
+  );
+};
+
+export default SessionNews;
